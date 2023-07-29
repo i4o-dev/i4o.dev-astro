8c452b6612ad7e8af82004113928df761000ea47
Ilango <rajagopalilango@gmail.com>
2023-07-29 12:58:26 +0530

    👌 IMPROVE: Change all heading font family to merriweather

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,7 @@ module.exports = {
             },
             fontFamily: {
                 sans: ["Inter", ...defaultTheme.fontFamily.sans],
+                serif: ["Merriweather", ...defaultTheme.fontFamily.serif],
             },
         },
     },
