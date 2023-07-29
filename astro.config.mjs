c2e613dd4ea62a2f53e8387c7e710a103e47972d
Ilango <rajagopalilango@gmail.com>
2023-07-29 17:43:46 +0530

    👌 IMPROVE: Add custom shiki theme and update post styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,28 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import { SITE_URL } from "./src/data/config";
-import react from "@astrojs/react";
-import mdx from "@astrojs/mdx";
-
-import partytown from "@astrojs/partytown";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import { SITE_URL } from './src/data/config'
+import react from '@astrojs/react'
+import mdx from '@astrojs/mdx'
+import partytown from '@astrojs/partytown'
+import theme from './shiki.json'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), sitemap(), react(), mdx(), partytown()],
+    integrations: [
+        tailwind(),
+        sitemap(),
+        react(),
+        mdx({ drafts: true }),
+        partytown(),
+    ],
     site: SITE_URL,
     markdown: {
-        syntaxHighlight: "shiki",
+        drafts: true,
+        syntaxHighlight: 'shiki',
         shikiConfig: {
-            theme: "nord",
+            theme: theme,
             wrap: false,
         },
     },
-});
+})
