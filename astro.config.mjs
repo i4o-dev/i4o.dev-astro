a0b97e8a481e96eca689708c6273cbefe8fc8150
Ilango <rajagopalilango@gmail.com>
2023-07-29 10:54:41 +0530

    📦 NEW: Add astro integrations - react, mdx, and partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
-import { SITE_URL } from './src/data/config'
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import { SITE_URL } from "./src/data/config";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), sitemap()],
-	site: SITE_URL,
-	markdown: {
-		syntaxHighlight: 'shiki',
-		shikiConfig: {
-			theme: 'nord',
-			wrap: false,
-		},
-	},
-})
+    integrations: [tailwind(), sitemap(), react(), mdx(), partytown()],
+    site: SITE_URL,
+    markdown: {
+        syntaxHighlight: "shiki",
+        shikiConfig: {
+            theme: "nord",
+            wrap: false,
+        },
+    },
+});
