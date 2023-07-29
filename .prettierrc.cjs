fabe87ee18143998186b2c6327a0f52b5b3488fb
Ilango <rajagopalilango@gmail.com>
2023-07-29 10:36:43 +0530

    👌 IMPROVE: Rename prettier config file

---
+++ .prettierrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+	bracketSpacing: true,
+	endOfLine: 'lf',
+	bracketSameLine: false,
+	jsxSingleQuote: true,
+	semi: false,
+	singleQuote: true,
+	tabWidth: 4,
+	trailingComma: 'es5',
+	useTabs: true,
+	plugins: [
+		require.resolve('prettier-plugin-tailwindcss', 'prettier-plugin-astro'),
+	],
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
+}
