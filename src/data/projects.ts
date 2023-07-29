3e48d7abf49527c45a3e0c60501bbf6691b89310
Ilango <rajagopalilango@gmail.com>
2023-07-29 12:44:06 +0530

    👌 IMPROVE: Update projects page and list

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,7 +5,7 @@ export type ProjectPresentation = {
 
 export type Project = {
     title: string
-    techs: string[]
+    description: string
     link: string
     isComingSoon?: boolean
     isBackburner?: boolean
@@ -15,24 +15,56 @@ export type Project = {
 export const data: ProjectPresentation = {
     title: 'Projects',
     description:
-        "I build small web applications and tools that solve problems that I have. Each project uses at least one technology that I'm trying to learn. All of my projects are *breakable toys*. They're all open-source (or will be open-sourced).",
+        "I build small web applications and tools that solve problems that I have. Each project uses at least one technology that I'm trying to learn. All of my projects are <a class='underline decoration-dashed underline-offset-8' href='https://i4o.dev/blog/learning-by-building-breakable-toys' target='_self'>breakable toys</a>. They're all *open-source* (or will be open-sourced).",
 }
 
 export const projects: Project[] = [
     {
         title: 'Aurelius',
-        techs: ['React', 'Remix', 'TypeScript'],
+        description: 'Minimal, open-source writing app',
+        // tech: ['React', 'Remix', 'TypeScript'],
         link: 'https://aurelius.ink/',
+        isFeatured: true,
     },
     {
         title: 'Catalyst UI',
-        techs: ['React', 'TypeScript', 'Radix UI', 'TailwindCSS'],
+        description: 'React component library built using Radix and Tailwind',
+        // tech: ['React', 'TypeScript', 'Radix UI', 'TailwindCSS'],
         link: 'https://catalyst-ui.com',
+        isFeatured: true,
     },
     {
         title: 'Rescribe',
-        techs: ['React', 'Remix', 'MDX', 'TailwindCSS'],
+        description: 'Docs and blog generator for Remix',
+        // tech: ['React', 'Remix', 'MDX', 'TailwindCSS'],
+        link: 'https://rescribe.site/',
+        isFeatured: true,
+    },
+    {
+        title: 'Synthwave Stack',
+        description: 'Remix Stack for quickly shipping web apps',
+        // tech: ['React', 'Remix', 'MDX', 'TailwindCSS'],
         link: 'https://rescribe.site/',
     },
+    {
+        title: 'cargo-use',
+        description:
+            'Cargo subcommand to start a new Rust project from a boilerplate/template repository',
+        // tech: ['Rust', 'clap.rs'],
+        link: 'https://github.com/i4o-dev/cargo-use',
+    },
+    {
+        title: 'Zilean',
+        description: 'Pomodoro CLI app written in Rust',
+        // tech: ['Rust', 'clap.rs'],
+        link: 'https://github.com/i4o-dev/zilean',
+        isComingSoon: true,
+    },
+    {
+        title: 'Herald',
+        description: 'Manage changelogs, roadmap, and banners from one dashboard',
+        // tech: ['React', 'Remix', 'MDX', 'TailwindCSS'],
+        link: 'https://rescribe.site/',
+        isComingSoon: true
+    },
 ]
-
