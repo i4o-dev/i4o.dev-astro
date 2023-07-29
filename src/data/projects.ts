4472dab73bf7647b60851daa14ab87510f9146ab
Ilango <rajagopalilango@gmail.com>
2023-07-29 12:13:25 +0530

    📦 NEW: Add projects page and update projects data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,26 +1,38 @@
+export type ProjectPresentation = {
+    title: string
+    description: string
+}
+
 export type Project = {
-    title: string;
-    techs: string[];
-    link: string;
-    isComingSoon?: boolean;
-};
+    title: string
+    techs: string[]
+    link: string
+    isComingSoon?: boolean
+    isBackburner?: boolean
+    isFeatured?: boolean
+}
 
-const projects: Project[] = [
-    {
-        title: "Aurelius",
-        techs: ["React", "Remix", "TypeScript"],
-        link: "https://aurelius.ink/",
-    },
-    {
-        title: "Catalyst UI",
-        techs: ["React", "TypeScript", "Radix UI", "TailwindCSS"],
-        link: "https://catalyst-ui.com",
-    },
-    {
-        title: "Rescribe",
-        techs: ["React", "Remix", "MDX", "TailwindCSS"],
-        link: "https://rescribe.site/",
-    },
-];
+export const data: ProjectPresentation = {
+    title: 'Projects',
+    description:
+        "I build small web applications and tools that solve problems that I have. Each project uses at least one technology that I'm trying to learn. All of my projects are *breakable toys*. They're all open-source (or will be open-sourced).",
+}
 
-export default projects;
+export const projects: Project[] = [
+    {
+        title: 'Aurelius',
+        techs: ['React', 'Remix', 'TypeScript'],
+        link: 'https://aurelius.ink/',
+    },
+    {
+        title: 'Catalyst UI',
+        techs: ['React', 'TypeScript', 'Radix UI', 'TailwindCSS'],
+        link: 'https://catalyst-ui.com',
+    },
+    {
+        title: 'Rescribe',
+        techs: ['React', 'Remix', 'MDX', 'TailwindCSS'],
+        link: 'https://rescribe.site/',
+    },
+]
+
