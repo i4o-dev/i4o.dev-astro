e5f980cb8bb251d70ba876537f231b7bf2f0d812
Ilango <rajagopalilango@gmail.com>
2023-07-30 09:13:00 +0530

    👌 IMPROVE: Add excerpt to book notes collection schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -14,6 +14,7 @@ const bookNotesCollection = defineCollection({
     type: 'content',
     schema: z.object({
         title: z.string(),
+        excerpt: z.string().default(''),
         author: z.string(),
         date_published: z.date(),
         draft: z.boolean().default(false),
