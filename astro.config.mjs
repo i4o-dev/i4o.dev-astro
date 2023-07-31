d1d2ba9370f8c4b4a8c1c58b1daf7947110bd525
Ilango <rajagopalilango@gmail.com>
2023-07-31 10:30:46 +0530

    📦 NEW: Add vercel integration and switch to ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,33 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
-import { SITE_URL } from './src/data/config'
 import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
 import partytown from '@astrojs/partytown'
+import vercel from '@astrojs/vercel/serverless'
+import { SITE_URL } from './src/data/config'
 import theme from './shiki.json'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        tailwind(),
-        sitemap(),
-        react(),
-        mdx({ drafts: true }),
-        partytown(),
-    ],
-    site: SITE_URL,
-    markdown: {
-        drafts: true,
-        syntaxHighlight: 'shiki',
-        shikiConfig: {
-            theme: theme,
-            wrap: false,
-        },
-    },
+	integrations: [
+		tailwind(),
+		sitemap(),
+		react(),
+		mdx({
+			drafts: true,
+		}),
+		partytown(),
+	],
+	site: SITE_URL,
+	markdown: {
+		drafts: true,
+		syntaxHighlight: 'shiki',
+		shikiConfig: {
+			theme: theme,
+			wrap: false,
+		},
+	},
+	output: 'server',
+	adapter: vercel(),
 })
