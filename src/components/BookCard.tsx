975ccfa3370d2a0d8923f57556b49291ef581eff
Ilango <rajagopalilango@gmail.com>
2023-08-15 11:16:32 +0530

    👌 IMPROVE: Convert bookcard astro component to react

---
+++ src/components/BookCard.tsx
@@ -0,0 +1,13 @@
+import type { Book } from '@/data/books'
+
+export default function BookCard({ cover }: Book) {
+    return (
+        <figure className='mb-8 overflow-hidden rounded-2xl rounded-bl-none rounded-tl-none shadow-xl'>
+            <img
+                className='h-full w-full object-cover'
+                loading='lazy'
+                src={cover}
+            />
+        </figure>
+    )
+}
