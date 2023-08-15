cd1f3dddba271e56941aa57ef764a77576392fd3
Ilango <rajagopalilango@gmail.com>
2023-08-15 11:11:05 +0530

    👌 IMPROVE: Convert some astro components to react

---
+++ src/components/shared/Link.tsx
@@ -0,0 +1,25 @@
+import type { AnchorHTMLAttributes } from 'react'
+
+type Props = AnchorHTMLAttributes<HTMLAnchorElement> & {
+    href: string
+    isUnderline?: boolean
+    label: string
+}
+
+export default function Link({ href, label, isUnderline, ...props }: Props) {
+    return (
+        <a
+            href={href}
+            className={`${isUnderline
+                    ? 'underline decoration-dashed underline-offset-8'
+                    : ''
+                } cursor-pointer hover:text-neutral-100`}
+            {...props}
+            rel='noreferrer'
+            target='_blank'
+        >
+            {label}
+            <span className='sr-only'>{label} link</span>
+        </a>
+    )
+}
